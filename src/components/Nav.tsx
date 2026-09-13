49f99a337214f786a7a43d3b8a350c01e94c096d
subongkarchakmama <subongkar.chakma@gmail.com>
2026-09-13 10:41:17 +0600

    Navbar is added on the top

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,23 @@ import logo from "../assets/logo-text.png"
 
 const Nav = () => {
     return (
-      <h2>Navbar</h2>
+       <nav className='my-4'>
+           <div className='container mx-auto flex justify-between'>
+            <img src={logo} alt="" />
+            <ul className='flex gap-4 items-center'>
+                <li>Home</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <div>
+                <button className="btn border-none font-normal bg-white">Sign In</button>
+                <button className="btn px-5 py-2 text-white bg-[#D91B7E] rounded-full">Sign Up</button>
+            </div>
+            </div>
+            <hr className='mt-3' /> 
+        </nav>
     );
 };
 
